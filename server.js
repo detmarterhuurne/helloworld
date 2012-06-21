d945be0cec0a9a8f22d06f5debe8d1003f1aa4e3
Detmar ter Huurne <detmar@c9.io>
2012-06-21 10:57:45 +0000

    Tjappies Mammies

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 require("http").createServer(function(req, res) {
     res.writeHead(200);
     res.end("Welcome to Amsterdam, detmar");
-}).listen(process.env.C9_PORT);
+}).listen(process.env.PORT);
