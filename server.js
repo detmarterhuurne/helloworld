feb39379902580a8d111e9953acdbe1a34820f25
Detmar ter Huurne <detmar@ajax.org>
2012-09-19 09:50:00 -0400

    added node chat

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 require("http").createServer(function(req, res) {
     res.writeHead(200);
-    res.end("Welcome to Amsterdam, detmar");
+    res.end("Welcome to Amsterdam, detmar and Bas");
+    console.log("hello");
 }).listen(process.env.PORT);
+
+
+console.log("Hello!:)");
+
